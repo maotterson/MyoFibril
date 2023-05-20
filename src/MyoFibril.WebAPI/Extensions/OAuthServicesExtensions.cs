3862d99846db404b5fce788752a42667a374c2f8
maotterson <otterson.2@gmail.com>
2023-05-20 00:51:27 -0400

    add di extension method for strava oauth services

---
+++ src/MyoFibril.WebAPI/Extensions/OAuthServicesExtensions.cs
@@ -0,0 +1,19 @@
+﻿using MyoFibril.WebAPI.OAuth.Interfaces;
+using MyoFibril.WebAPI.OAuth;
+
+namespace MyoFibril.WebAPI.Extensions;
+
+public static class OAuthServicesExtensions
+{
+    public static void AddStravaOAuthServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHttpClient("strava-oauth", httpClient =>
+        {
+            httpClient.BaseAddress = new Uri(configuration["StravaApp:RequestUri"]!);
+        });
+        services.AddScoped<ITokenCache, MemoryTokenCache>();
+        services.AddScoped<ITokenRefreshService, TokenRefreshService>();
+        services.AddScoped<IOAuthService, OAuthService>();
+        services.AddTransient<AuthHeaderHandler>();
+    }
+}
--- src/MyoFibril.WebAPI/Program.cs
+++ src/MyoFibril.WebAPI/Program.cs
@@ -1,11 +1,16 @@
+using MyoFibril.WebAPI.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register Strava OAuth Services
+builder.Services.AddStravaOAuthServices(configuration);
 
 var app = builder.Build();
 
