48779af1339240438b843ca60ae0bf69f03d4a30
maotterson <otterson.2@gmail.com>
2023-05-20 00:43:25 -0400

    add new access token properties for oauth flow

--- src/MyoFibril.Contracts/Strava/Responses/OAuth/NewAccessTokenResponse.cs
+++ src/MyoFibril.Contracts/Strava/Responses/OAuth/NewAccessTokenResponse.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace MyoFibril.Contracts.Strava.Responses.OAuth;
-public class NewAccessTokenResponse
+public record TokenResponse
 {
+    [JsonPropertyName("token_type")]
+    public string TokenType { get; init; } = default!;
+
+    [JsonPropertyName("access_token")]
+    public string AccessToken { get; init; } = default!;
+
+    [JsonPropertyName("expires_at")]
+    public long ExpiresAt { get; init; }
+
+    [JsonPropertyName("expires_in")]
+    public int ExpiresIn { get; init; }
+
+    [JsonPropertyName("refresh_token")]
+    public string RefreshToken { get; init; } = default!;
 }
