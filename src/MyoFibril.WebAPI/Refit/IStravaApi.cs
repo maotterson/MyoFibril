2dec1ed182d435f9eeb42f98c5481b11a672acc3
maotterson <otterson.2@gmail.com>
2023-05-21 01:38:55 -0400

    bring over refit api

---
+++ src/MyoFibril.WebAPI/Refit/IStravaApi.cs
@@ -0,0 +1,16 @@
+﻿using MyoFibril.Contracts.Strava.CreateActivity;
+using Refit;
+
+namespace StravaWithRefitSimple.Api;
+
+[Headers("Authorization: Bearer")]
+public interface IStravaApi
+{
+    [Headers("Accept: application/json")]
+    [Get("/activities/{id}")]
+    Task<ApiResponse<GetActivityResponse>> GetActivityById(string id);
+
+    [Headers("Content-Type: application/json")]
+    [Post("/activities")]
+    Task<ApiResponse<CreateActivityResponse>> CreateActivity([Body(BodySerializationMethod.Serialized)] CreateActivityRequest activity);
+}
