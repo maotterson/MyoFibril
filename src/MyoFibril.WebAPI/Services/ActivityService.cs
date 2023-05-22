ade4546bcfbf0c63692a186002ac40488bb591c8
maotterson <otterson.2@gmail.com>
2023-05-22 00:10:04 -0400

    add extension method to build sample strava create contract from incoming request

--- src/MyoFibril.WebAPI/Services/ActivityService.cs
+++ src/MyoFibril.WebAPI/Services/ActivityService.cs
@@ -1,7 +1,32 @@
-﻿using MyoFibril.WebAPI.Services.Interfaces;
+﻿using MyoFibril.Contracts.WebAPI.CreateActivity;
+using MyoFibril.Contracts.WebAPI.GetActivity;
+using MyoFibril.WebAPI.Services.Interfaces;
+using MyoFibril.WebAPI.Strava.Services.Interfaces;
 
 namespace MyoFibril.WebAPI.Services;
 
 public class ActivityService : IActivityService
 {
+    private readonly IStravaActivityService _stravaActivityService;
+    public ActivityService(IStravaActivityService stravaActivityService)
+    {
+        _stravaActivityService = stravaActivityService;
+    }
+
+    public async Task<CreateActivityResponse> CreateActivity(CreateActivityRequest request)
+    {
+        var stravaRequest = request.AsStravaCreateActivityRequest();
+        var stravaResponse = await _stravaActivityService.CreateActivity(stravaRequest);
+        var response = new CreateActivityResponse
+        {
+            Id = Guid.NewGuid(),
+            Name = request.Name,
+            StravaActivity = stravaResponse,
+        };
+        return response;
+    }
+
+    public async Task<GetActivityResponse> GetActivityById(string id)
+    {
+    }
 }
