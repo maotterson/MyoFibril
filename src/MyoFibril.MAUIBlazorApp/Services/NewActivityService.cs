c0583f1ecae0899068c54149be7d7afffa6d1b7c
maotterson <otterson.2@gmail.com>
2023-05-22 02:01:13 -0400

    add basic flow to test http request to running server

--- src/MyoFibril.MAUIBlazorApp/Services/NewActivityService.cs
+++ src/MyoFibril.MAUIBlazorApp/Services/NewActivityService.cs
@@ -1,5 +1,6 @@
 ﻿using MyoFibril.Contracts.WebAPI.CreateActivity;
 using System.Net.Http;
+using System.Text;
 
 namespace MyoFibril.MAUIBlazorApp.Services;
 public class NewActivityService : INewActivityService
@@ -11,6 +12,20 @@ public class NewActivityService : INewActivityService
     }
     public async Task<CreateActivityResponse> CreateActivity(CreateActivityRequest createActivityRequest)
     {
-        throw new NotImplementedException();
+        var httpClient = _httpClientFactory.CreateClient();
+        var requestUri = "https://localhost:7152/Activity";
+        var jsonContent = new StringContent(System.Text.Json.JsonSerializer.Serialize(createActivityRequest), Encoding.UTF8, "application/json");
+
+        var response = await httpClient.PostAsync(requestUri, jsonContent);
+
+        // Check if the response is successful
+        response.EnsureSuccessStatusCode();
+
+        // Deserialize the response content to the desired type
+        var responseBody = await response.Content.ReadAsStringAsync();
+        var createActivityResponse = System.Text.Json.JsonSerializer.Deserialize<CreateActivityResponse>(responseBody);
+
+        // Return the deserialized response
+        return createActivityResponse;
     }
 }
