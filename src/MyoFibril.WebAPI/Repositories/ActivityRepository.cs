badef0410cba2e64d0c8a922aacbdff99d3078d1
maotterson <otterson.2@gmail.com>
2023-05-22 00:11:50 -0400

    create repository layer for local persistence on webapi level

---
+++ src/MyoFibril.WebAPI/Repositories/ActivityRepository.cs
@@ -0,0 +1,7 @@
+﻿using MyoFibril.WebAPI.Repositories.Interfaces;
+
+namespace MyoFibril.WebAPI.Repositories;
+
+public class ActivityRepository : IActivityRepository
+{
+}
---
+++ src/MyoFibril.WebAPI/Repositories/Interfaces/IActivityRepository.cs
@@ -0,0 +1,10 @@
+﻿using MyoFibril.Contracts.WebAPI.CreateActivity;
+using MyoFibril.Contracts.WebAPI.GetActivity;
+
+namespace MyoFibril.WebAPI.Repositories.Interfaces;
+
+public interface IActivityRepository
+{
+    Task<CreateActivityResponse> CreateActivity(CreateActivityRequest request);
+    Task<GetActivityResponse> GetActivityById(string id);
+}
