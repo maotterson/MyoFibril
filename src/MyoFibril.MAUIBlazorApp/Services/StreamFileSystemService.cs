7a942c80c678ab44d92439fb9b85dcff98b75632
maotterson <otterson.2@gmail.com>
2023-05-27 02:10:10 -0400

    add filesystemservice implementation for maui app to maui app

---
+++ src/MyoFibril.MAUIBlazorApp/Services/StreamFileSystemService.cs
@@ -0,0 +1,24 @@
+﻿using MyoFibril.SharedData.Services;
+using System.Reflection;
+
+namespace MyoFibril.MAUIBlazorApp.Services;
+public class StreamFileSystemService : IFileSystemService
+{
+    public async Task<string> LoadFileAsStringAsync(string path)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        using (Stream stream = assembly.GetManifestResourceStream(path))
+        {
+            if (stream == null)
+            {
+                throw new FileNotFoundException($"Resource '{path}' not found.");
+            }
+
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string fileContent = await reader.ReadToEndAsync();
+                return fileContent;
+            }
+        }
+    }
+}
