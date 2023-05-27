5d89bfbb1629227fb04319469f68010ea0802f4a
maotterson <otterson.2@gmail.com>
2023-05-27 18:29:24 -0400

    add missing reference for describe exercise component

--- src/MyoFibril.MAUIBlazorApp/Services/Api/INewActivityService.cs
+++ src/MyoFibril.MAUIBlazorApp/Services/Api/INewActivityService.cs
@@ -1,6 +1,6 @@
 ﻿using MyoFibril.Contracts.WebAPI.CreateActivity;
 
-namespace MyoFibril.MAUIBlazorApp.Services;
+namespace MyoFibril.MAUIBlazorApp.Services.Api;
 public interface INewActivityService
 {
     Task<CreateActivityResponse> CreateActivity(CreateActivityRequest createActivityRequest);
