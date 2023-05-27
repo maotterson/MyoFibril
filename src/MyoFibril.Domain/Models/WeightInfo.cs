e9b8aefb5f5729aab30b76b6eb6d3e539c287366
maotterson <otterson.2@gmail.com>
2023-05-27 18:14:51 -0400

    add model to represent weight info + util class for basic conversion operations

---
+++ src/MyoFibril.Domain/Models/WeightInfo.cs
@@ -0,0 +1,8 @@
+﻿using MyoFibril.Domain.Enums;
+
+namespace MyoFibril.Domain.Models;
+public class WeightInfo
+{
+    public double Value { get; set; }
+    public WeightInfoUnit Units { get; set; } 
+}
---
+++ src/MyoFibril.Domain/Utils/WeightInfoUtils.cs
@@ -0,0 +1,42 @@
+﻿using MyoFibril.Domain.Entities;
+using MyoFibril.Domain.Enums;
+using MyoFibril.Domain.Models;
+
+namespace MyoFibril.Domain.Utils;
+public static class WeightInfoUtils
+{
+    public static void ConvertToMetricUnits(this PerformedExerciseEntity performedExercise)
+    {
+        foreach(var set in performedExercise.Sets)
+        {
+            set.ConvertTo(WeightInfoUnit.Kilograms);
+        }
+    }
+    public static void ConvertToImperialUnits(this PerformedExerciseEntity performedExercise)
+    {
+        foreach (var set in performedExercise.Sets)
+        {
+            set.ConvertTo(WeightInfoUnit.Pounds);
+        }
+    }
+
+    private static void ConvertTo(this SetInfo set, WeightInfoUnit unit)
+    {
+        if (set.Weight!.Units == unit) return;
+
+        switch (unit)
+        {
+            case WeightInfoUnit.Kilograms:
+                set.Weight!.Value /= 2.2;
+                break;
+            case WeightInfoUnit.Pounds:
+                set.Weight!.Value *= 2.2;
+                break;
+            default:
+                throw new Exception($"Unknown {typeof(WeightInfoUnit)}: {unit}");
+        }
+
+        set.Weight!.Value 
+        set.Weight!.Units = unit;
+    }
+}
