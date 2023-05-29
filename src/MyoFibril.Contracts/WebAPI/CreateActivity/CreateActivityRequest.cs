420d6904e863b977040a8caa011044b44725dc99
maotterson <otterson.2@gmail.com>
2023-05-29 02:39:04 -0400

    pass collection of performedexercises to activity request (can refactor to dto)

--- src/MyoFibril.Contracts/WebAPI/CreateActivity/CreateActivityRequest.cs
+++ src/MyoFibril.Contracts/WebAPI/CreateActivity/CreateActivityRequest.cs
@@ -1,9 +1,14 @@
-﻿using System.Text.Json.Serialization;
+﻿using MyoFibril.Domain.Entities;
+using System.Text.Json.Serialization;
 
 namespace MyoFibril.Contracts.WebAPI.CreateActivity;
 public class CreateActivityRequest
 {
     [JsonPropertyName("name")]
     public string Name { get; set; } = string.Empty;
+    [JsonPropertyName("date-created")]
+    public DateTimeOffset DateCreated { get; set; }
+    [JsonPropertyName("performed-exercises")]
+    public List<PerformedExerciseEntity>? PerformedExercises { get; set; }
 
 }
