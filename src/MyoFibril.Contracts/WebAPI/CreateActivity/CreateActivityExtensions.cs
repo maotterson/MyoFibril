40f39b7130f40bdd2fdedf3aa29d77c54006f277
maotterson <otterson.2@gmail.com>
2023-05-31 01:23:27 -0400

    implement a strava description builder to describe workout

--- src/MyoFibril.Contracts/WebAPI/CreateActivity/CreateActivityExtensions.cs
+++ src/MyoFibril.Contracts/WebAPI/CreateActivity/CreateActivityExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using MyoFibril.Contracts.Strava.CreateActivity;
 using MyoFibril.Contracts.Strava.Models;
+using System.Text;
 
 namespace MyoFibril.Contracts.WebAPI.CreateActivity;
 public static class CreateActivityExtensions
@@ -10,11 +11,63 @@ public static class CreateActivityExtensions
         var stravaRequest = new StravaCreateActivityRequest
         {
             Name = request.Name,
-            Description = "Created by web api",
+            Description = request.BuildStravaDescription(),
             SportType = StravaSportType.Workout.ToString(),
-            StartDateLocal = DateTimeOffset.Now.ToLocalTime().ToString("o"),
-            ElapsedTime = 333
+            StartDateLocal = request.DateCreated.ToLocalTime().ToString("o"),
+            ElapsedTime = 3600
         };
         return stravaRequest;
+    }
+
+    private static string BuildStravaDescription(this CreateActivityRequest request)
+    {
+        StringBuilder sb = new StringBuilder();
+        if (request.PerformedExercises is null) return sb.ToString();
+
+        // Variations:
+
+        // Squats
+        // Squats: N/A
+        // Squats: x15, x15
+        // Squats: 225x15, 225x15
+        // Squats: 225x15 (note), x10 (note), N/A (note)
+        foreach (var ex in request.PerformedExercises)
+        {
+            sb.Append($"{ex.Exercise.Name})");
+            if (ex.Sets.Count == 0)
+            {
+                sb.Append($"\n");
+                continue;
+            }
+
+            // output each set detail
+            foreach (var set in ex.Sets)
+            {
+                if (set.Reps is null || set.Reps == 0)
+                {
+                    sb.Append($": N/A");
+                }
+                else if (set.Weight is null)
+                {
+                    sb.Append($": x{set.Reps}");
+                }
+                else
+                {
+                    sb.Append($": {set.Weight}x{set.Reps}");
+                }
+
+                if(set.AdditionalDetails is not null && !string.IsNullOrEmpty(set.AdditionalDetails))
+                {
+                    sb.Append($" ({set.AdditionalDetails})");
+                }
+
+                sb.Append(", ");
+            }
+
+            // trim trailing space and comma and go to new line
+            sb.Remove(sb.Length - 1, 2);
+            sb.Append('\n');
+        }
+        return sb.ToString();
     }
 }
