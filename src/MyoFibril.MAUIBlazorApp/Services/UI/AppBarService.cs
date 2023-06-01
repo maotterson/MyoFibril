34af6f90a178962d7bcbede716e6bf91bc35fc88
maotterson <otterson.2@gmail.com>
2023-06-01 02:03:25 -0400

    create service to manage the app bar visibility and content

---
+++ src/MyoFibril.MAUIBlazorApp/Services/UI/AppBarService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MyoFibril.MAUIBlazorApp.Services.UI;
+public class AppBarService : IAppBarService
+{
+    private NavigationManager _navigationManager;
+    private Dictionary<string, string> _pageTitles = new Dictionary<string, string>()
+    {
+        { "/", "" },
+        { "/NewActivity", "New Activity" },
+        { "/Activities", "View Activities" }
+    };
+    public AppBarService(NavigationManager navManager)
+    {
+        _navigationManager = navManager;
+    }
+    public string GetAppBarTitle()
+    {
+        var uri = _navigationManager.Uri;
+        return _pageTitles.GetValueOrDefault(uri);
+    }
+
+    public bool IsAppBarVisible()
+    {
+        return !string.IsNullOrEmpty(GetAppBarTitle());
+    }
+}
