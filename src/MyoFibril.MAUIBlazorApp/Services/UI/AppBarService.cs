740d60d1ef3497d147b161a1be78ef72865a1d1d
maotterson <otterson.2@gmail.com>
2023-06-01 02:38:40 -0400

    set visibility of app bar using navigationmanager on mainlayout

--- src/MyoFibril.MAUIBlazorApp/Services/UI/AppBarService.cs
+++ src/MyoFibril.MAUIBlazorApp/Services/UI/AppBarService.cs
@@ -6,18 +6,22 @@ public class AppBarService : IAppBarService
     private NavigationManager _navigationManager;
     private Dictionary<string, string> _pageTitles = new Dictionary<string, string>()
     {
-        { "/", "" },
-        { "/NewActivity", "New Activity" },
-        { "/Activities", "View Activities" }
+        { "", "" },
+        { "NewActivity", "New Activity" },
+        { "Activities", "View Activities" }
     };
     public AppBarService(NavigationManager navManager)
     {
         _navigationManager = navManager;
     }
+
     public string GetAppBarTitle()
     {
         var uri = _navigationManager.Uri;
-        return _pageTitles.GetValueOrDefault(uri);
+        var relativePath = _navigationManager.ToBaseRelativePath(uri);
+        var title = _pageTitles.GetValueOrDefault(relativePath);
+
+        return title;
     }
 
     public bool IsAppBarVisible()
