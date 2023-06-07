f2c6660f1dfad59925809e5acaa55f4a0f8c676d
maotterson <otterson.2@gmail.com>
2023-06-07 01:40:30 -0400

    add implementation for storage service

--- src/MyoFibril.MAUIBlazorApp/Services/Local/IStorageService.cs
+++ src/MyoFibril.MAUIBlazorApp/Services/Local/IStorageService.cs
@@ -1,5 +1,7 @@
 ﻿namespace MyoFibril.MAUIBlazorApp.Services.Local;
 public interface IStorageService
 {
-    Task<T> GetItemAsync<T>(string path);
+    Task StoreItemAsync<T>(string key, T item);
+    Task<T> GetItemAsync<T>(string key);
+    void RemoveItem(string key);
 }
--- src/MyoFibril.MAUIBlazorApp/Services/Local/StorageService.cs
+++ src/MyoFibril.MAUIBlazorApp/Services/Local/StorageService.cs
@@ -1,5 +1,25 @@
-﻿namespace MyoFibril.MAUIBlazorApp.Services.Local;
+﻿using CoreData;
+using System.Text.Json;
+
+namespace MyoFibril.MAUIBlazorApp.Services.Local;
 public class StorageService : IStorageService
 {
-    GetItemAsync
+    public async Task StoreItemAsync<T>(string key, T item)
+    {
+        var json = JsonSerializer.Serialize(item);
+        await SecureStorage.SetAsync(key, json);
+    }
+
+    public async Task<T> GetItemAsync<T>(string key)
+    {
+        var json = await SecureStorage.GetAsync(key);
+        if (json is null) return default(T);
+        return JsonSerializer.Deserialize<T>(json);
+    }
+
+    public void RemoveItem(string key)
+    {
+        SecureStorage.Remove(key);
+    }
 }
+
