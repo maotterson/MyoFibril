3b04b44a3faedb51c23681b1e02d1ebacaf6846b
maotterson <otterson.2@gmail.com>
2023-06-09 01:05:23 -0400

    add mongodb driver to webapi

---
+++ src/MyoFibril.WebAPI/Repositories/CredentialsRepository.cs
@@ -0,0 +1,19 @@
+﻿using MyoFibril.Domain.Entities.Auth;
+using MyoFibril.WebAPI.Repositories.Interfaces;
+
+namespace MyoFibril.WebAPI.Repositories;
+
+public class CredentialsRepository : ICredentialsRepository
+{
+    private const string COLLECTION_NAME = "credentials";
+
+    public CredentialsRepository()
+    {
+        
+    }
+
+    public async Task<UserCredentialsEntity> GetCredentialsForUsername(string username)
+    {
+
+    }
+}
