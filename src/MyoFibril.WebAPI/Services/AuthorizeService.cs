0cc20677e7dd693ceaf51fa5c0f8cf0fd12979d1
maotterson <otterson.2@gmail.com>
2023-06-09 00:21:14 -0400

    create implementation for authorizetoken in service layer

--- src/MyoFibril.WebAPI/Services/AuthorizeService.cs
+++ src/MyoFibril.WebAPI/Services/AuthorizeService.cs
@@ -15,15 +15,56 @@ public class AuthorizeService : IAuthorizeService
         _authorizeRepository = authorizeRepository;
     }
 
-    public Task<bool> AuthorizeToken(AuthorizeTokenRequest authorizeTokenRequest)
+    public async Task<AuthorizeTokenResponse> AuthorizeToken(AuthorizeTokenRequest authorizeTokenRequest)
     {
-        var token = authorizeTokenRequest.AccessToken;
+        var tokenToAuthorize = authorizeTokenRequest.AccessToken;
+        var refreshTokenToAuthorize = authorizeTokenRequest.RefreshToken;
+
+        GetAccessTokenResponse? tokenInfo = default;
+        UserInfo? userInfo = default;
+        var isVerified = _authorizeRepository.isVerifiedToken(tokenToAuthorize);
+
+        if (isVerified)
+        {
+            tokenInfo = new GetAccessTokenResponse
+            {
+                AccessToken = tokenToAuthorize,
+                RefreshToken = refreshTokenToAuthorize
+            };
+            userInfo = new UserInfo
+            {
+                Username = "todo: get username from user repository"
+            };
+        }
+
+        var accessToken = _authorizeRepository.GetAccessTokenWithRefreshToken(refreshTokenToAuthorize);
+        if(accessToken is not null)
+        {
+            isVerified = true;
+            tokenInfo = new GetAccessTokenResponse
+            {
+                AccessToken = accessToken,
+                RefreshToken = refreshTokenToAuthorize
+            };
+            userInfo = new UserInfo
+            {
+                Username = "todo: get username from user repository"
+            };
+        }
+
+        return new AuthorizeTokenResponse
+        {
+            IsAuthorized = isVerified,
+            TokenInfo = tokenInfo,
+            UserInfo = userInfo
+        };
 
     }
 
     public Task<GetAccessTokenResponse> GetAccessTokenWithRefreshToken(GetTokenWithRefreshTokenRequest request)
     {
         var refreshToken = request.RefreshToken;
+        var accessToken = _authorizeRepository.GetAccessTokenWithRefreshToken(refreshTokenToAuthorize);
     }
 
     public Task<GetAccessTokenResponse> GetAccessTokenWithUserCredentials(GetTokenWithUserCredentialsRequest request)
