b793024f91b8d663a50f089e7924f6870777ba00
maotterson <otterson.2@gmail.com>
2023-06-10 23:41:33 -0400

    add jwt.net + create jwtservice layout

---
+++ src/MyoFibril.WebAPI/Services/JwtService.cs
@@ -0,0 +1,28 @@
+﻿using MyoFibril.Domain.Entities.Auth;
+using MyoFibril.WebAPI.Models.Auth;
+using MyoFibril.WebAPI.Services.Interfaces;
+
+namespace MyoFibril.WebAPI.Services;
+
+public class JwtService : IJwtService
+{
+    public Task<string> GetAccessTokenWithRefreshToken(string refreshToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<(string accessToken, string refreshToken)> GetTokensWithCredentials(UserCredentialsEntity credentials)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> VerifyCredentials(UserCredentialsEntity storedCredentials, ProtectedUserCredentials credentialsToVerify)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> VerifyToken(string accessToken)
+    {
+        throw new NotImplementedException();
+    }
+}
