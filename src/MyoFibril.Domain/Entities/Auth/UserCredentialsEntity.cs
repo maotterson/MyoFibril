970fd964078b7bcfb2c2aabb786d64de2aac35ec
maotterson <otterson.2@gmail.com>
2023-06-13 01:18:35 -0400

    add bson identifier to credentials entity for mongo compatability

--- src/MyoFibril.Domain/Entities/Auth/UserCredentialsEntity.cs
+++ src/MyoFibril.Domain/Entities/Auth/UserCredentialsEntity.cs
@@ -1,6 +1,12 @@
-﻿namespace MyoFibril.Domain.Entities.Auth;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MyoFibril.Domain.Entities.Auth;
 public class UserCredentialsEntity
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
     public string Username { get; set; } = default!;
     public string HashedPassword { get; set; } = default!;
     public string Salt { get; set; } = default!;
