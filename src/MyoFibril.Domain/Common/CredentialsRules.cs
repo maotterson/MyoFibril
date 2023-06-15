2578cc1e1e4ad5f2c03fead0d7ddfc1c6617524c
maotterson <otterson.2@gmail.com>
2023-06-14 22:39:36 -0400

    refactor -> change credentials utils to validator class

---
+++ src/MyoFibril.Domain/Common/CredentialsRules.cs
@@ -0,0 +1,9 @@
+﻿namespace MyoFibril.Domain.Common;
+public static class CredentialsRules
+{
+
+    public const int MIN_USERNAME_LENGTH = 6;
+    public const int MIN_PASSWORD_LENGTH = 6;
+    public const int MAX_USERNAME_LENGTH = 20;
+    public const int MAX_PASSWORD_LENGTH = 30;
+}
---
+++ src/MyoFibril.Domain/Common/CredentialsValidator.cs
@@ -0,0 +1,44 @@
+﻿namespace MyoFibril.Domain.Utils;
+
+using MyoFibril.Domain.Common;
+using System.Net.Mail;
+public static class CredentialsValidator
+{
+
+    public static bool ValidateUsername(string username)
+    {
+        if (string.IsNullOrEmpty(username)) return false;
+        if (username.Length < CredentialsRules.MAX_USERNAME_LENGTH) return false;
+        if (username.Length > CredentialsRules.MAX_USERNAME_LENGTH) return false;
+        return true;
+    }
+
+    public static bool ValidatePasswordAndConfirmPassword(string password, string confirmPassword)
+    {
+        if (string.IsNullOrEmpty(password)) return false;
+        if (password.Length < CredentialsRules.MIN_PASSWORD_LENGTH) return false;
+        if (password.Length > CredentialsRules.MAX_PASSWORD_LENGTH) return false;
+        if (password != confirmPassword) return false;
+        return true;
+    }
+
+    public static bool ValidateEmail(string email)
+    {
+        if (string.IsNullOrEmpty(email)) return false;
+        if (!IsValidEmail(email)) return false;
+        return true;
+    }
+
+    private static bool IsValidEmail(string email)
+    {
+        try
+        {
+            var mailAddress = new MailAddress(email);
+            return true;
+        }
+        catch (FormatException)
+        {
+            return false;
+        }
+    }
+}
