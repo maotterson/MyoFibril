92154fe83f8891a750e37d036ebb425ce64c1e35
maotterson <otterson.2@gmail.com>
2023-06-14 22:20:50 -0400

    create utility class on domain layer for credentialsvalidation

---
+++ src/MyoFibril.Domain/Utils/CredentialsUtils.cs
@@ -0,0 +1,18 @@
+﻿namespace MyoFibril.Domain.Utils;
+public static class CredentialsUtils
+{
+    public static bool ValidateUsername(string username)
+    {
+
+    }
+
+    public static bool ValidatePasswordAndConfirmPassword(string password, string confirmPassword)
+    {
+
+    }
+
+    public static bool ValidateEmail(string email)
+    { 
+
+    }
+}
