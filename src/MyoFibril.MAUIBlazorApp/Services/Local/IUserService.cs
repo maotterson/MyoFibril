8fe60b48bdbfd2193250f2c556e7671367d99a1a
maotterson <otterson.2@gmail.com>
2023-06-14 23:40:59 -0400

    create contract for createaccountwithcredentials method

--- src/MyoFibril.MAUIBlazorApp/Services/Local/IUserService.cs
+++ src/MyoFibril.MAUIBlazorApp/Services/Local/IUserService.cs
@@ -1,6 +1,10 @@
-﻿namespace MyoFibril.MAUIBlazorApp.Services.Local;
+﻿using MyoFibril.Contracts.WebAPI.Auth;
+using MyoFibril.Contracts.WebAPI.Auth.Models;
+
+namespace MyoFibril.MAUIBlazorApp.Services.Local;
 public interface IUserService
 {
     Task Logout();
     Task<bool> CheckLoginCredentials(string username, string password);
+    Task<RegisterNewUserResponse> CreateAccountWithCredentials(UserRegisterCredentials credentials);
 }
