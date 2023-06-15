e79c4d263eef7a45e008021e276312f170066862
maotterson <otterson.2@gmail.com>
2023-06-14 23:17:35 -0400

    implement validator class in register component

--- src/MyoFibril.Domain/Common/CredentialsValidator.cs
+++ src/MyoFibril.Domain/Common/CredentialsValidator.cs
@@ -6,6 +6,7 @@ public static class CredentialsValidator
     private static string INVALID_PASSWORD_LENGTH_ERROR = $"Invalid password. Password must be between {CredentialsRules.MIN_PASSWORD_LENGTH} and {CredentialsRules.MAX_PASSWORD_LENGTH} characters.";
     private static string INVALID_EMAIL_ERROR = $"Invalid email. Please enter a valid email address.";
     private static string INVALID_CONFIRM_PASSWORD = $"Invalid password. Password and confirm password do not match.";
+
     public static (bool isValid, string? errorMessage) ValidateUsername(string username)
     {
         if (string.IsNullOrEmpty(username)) return (false, INVALID_USERNAME_LENGTH_ERROR);
