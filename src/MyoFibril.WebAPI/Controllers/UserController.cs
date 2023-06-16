30a7d70a25bf0a628c90054edea98d4f1f85ae10
maotterson <otterson.2@gmail.com>
2023-06-16 00:20:23 -0400

    tweak response return type from usercontroller get user

--- src/MyoFibril.WebAPI/Controllers/UserController.cs
+++ src/MyoFibril.WebAPI/Controllers/UserController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyoFibril.Contracts.WebAPI.Auth;
+using MyoFibril.WebAPI.Services.Interfaces;
 
 namespace MyoFibril.WebAPI.Controllers;
 
@@ -6,5 +8,19 @@ namespace MyoFibril.WebAPI.Controllers;
 [Route("[controller]")]
 public class UserController : ControllerBase
 {
-    
+    private readonly ILogger<UserController> _logger;
+    private readonly IUserService _userService;
+
+    public UserController(ILogger<UserController> logger, IUserService userService)
+    {
+        _logger = logger;
+        _userService = userService;
+    }
+
+    [HttpGet("{username}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(GetUserResponse))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult> GetUserByUsername(string username)
+    {
+    }
 }
