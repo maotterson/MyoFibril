7a983eb9577acf36952d1173d0c981147113472f
maotterson <otterson.2@gmail.com>
2023-06-17 00:02:49 -0400

    add creation of user entity to register process

--- src/MyoFibril.WebAPI/Services/RegisterService.cs
+++ src/MyoFibril.WebAPI/Services/RegisterService.cs
@@ -1,6 +1,7 @@
 ﻿using MyoFibril.Contracts.WebAPI.Auth;
 using MyoFibril.Contracts.WebAPI.Auth.Exceptions;
 using MyoFibril.Contracts.WebAPI.Auth.Models;
+using MyoFibril.Domain.Entities;
 using MyoFibril.WebAPI.Models.Auth;
 using MyoFibril.WebAPI.Repositories.Interfaces;
 using MyoFibril.WebAPI.Services.Interfaces;
@@ -10,11 +11,13 @@ namespace MyoFibril.WebAPI.Services;
 public class RegisterService : IRegisterService
 {
     private readonly ICredentialsRepository _credentialsRepository;
+    private readonly IUserRepository _userRepository;
     private readonly IJwtService _jwtService;
 
-    public RegisterService(ICredentialsRepository credentialsRepository, IJwtService jwtService)
+    public RegisterService(ICredentialsRepository credentialsRepository, IUserRepository userRepository, IJwtService jwtService)
     {
         _credentialsRepository = credentialsRepository;
+        _userRepository = userRepository;
         _jwtService = jwtService;
 
     }
@@ -34,6 +37,15 @@ public class RegisterService : IRegisterService
         var generatedSalt = BCrypt.Net.BCrypt.GenerateSalt();
         var protectedCredentials = new ProtectedUserCredentials(username: request.Username, password: request.Password, salt: generatedSalt, email: request.Email);
         await _credentialsRepository.RegisterNewUserWithProtectedCredentials(protectedCredentials);
+
+        // add created user to database
+        var user = new UserEntity
+        {
+            Username = request.Username,
+            Email = request.Email,
+            StravaAccount = null
+        };
+        await _userRepository.CreateUser(user);
 
         // use credentials to get an access and refresh token
         var createdCredentials = await _credentialsRepository.GetCredentialsForUsername(protectedCredentials.Username);
