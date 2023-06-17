98f1cce1e313aa5e16b969e1e120aefe0ea2e2af
maotterson <otterson.2@gmail.com>
2023-06-16 23:58:10 -0400

    create method for createuser on userrepository

--- src/MyoFibril.WebAPI/Repositories/Interfaces/IUserRepository.cs
+++ src/MyoFibril.WebAPI/Repositories/Interfaces/IUserRepository.cs
@@ -5,4 +5,5 @@ namespace MyoFibril.WebAPI.Repositories.Interfaces;
 public interface IUserRepository
 {
     Task<UserEntity> GetUserByUsername(string username);
+    Task CreateUser(UserEntity user);
 }
