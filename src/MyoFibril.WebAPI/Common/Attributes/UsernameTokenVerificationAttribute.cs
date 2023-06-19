c8b512e5ddbf4d5be1bb4ad46504a1c0bf25f728
maotterson <otterson.2@gmail.com>
2023-06-19 01:17:24 -0400

    fix username verification attribute by extracting jwtservice from httpcontext instead of di

--- src/MyoFibril.WebAPI/Common/Attributes/UsernameTokenVerificationAttribute.cs
+++ src/MyoFibril.WebAPI/Common/Attributes/UsernameTokenVerificationAttribute.cs
@@ -2,25 +2,22 @@
 using Microsoft.AspNetCore.Mvc;
 using MyoFibril.WebAPI.Services.Interfaces;
 using MyoFibril.Contracts.WebAPI.Auth.Exceptions;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace MyoFibril.WebAPI.Common.Attributes;
 
 public class UsernameTokenVerificationAttribute : ActionFilterAttribute
 {
-    private readonly IJwtService _jwtService;
-    public UsernameTokenVerificationAttribute(IJwtService jwtService)
-    {
-        _jwtService = jwtService;
-    }
 
     public override void OnActionExecuting(ActionExecutingContext context)
     {
         try
         {
+            var jwtService = context.HttpContext.RequestServices.GetService<IJwtService>()!;
             string accessToken = context.HttpContext.Request.Headers["Authorization"]!;
             string providedUsername = context.HttpContext.Request.Query["username"]!;
 
-            var isValidProvidedUsername = _jwtService.VerifyTokenAgainstUsername(accessToken, providedUsername);
+            var isValidProvidedUsername = jwtService.VerifyTokenAgainstUsername(accessToken, providedUsername);
             if (!isValidProvidedUsername) throw new UsernameTokenMismatchException();
             base.OnActionExecuting(context);
         }
